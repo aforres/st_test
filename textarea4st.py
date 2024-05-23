f36dab267f1674b3eb3d6d34f7117682baa26680
aforres <aforres@users.noreply.github.com>
2024-05-23 10:17:53 +1000

    Add files via upload

--- textarea4st.py
+++ textarea4st.py
@@ -5,10 +5,48 @@ st.title('DRAFT ONLY Resources News')
 nav=st.sidebar.radio('Navigation',['Home','Agriculture','Energy'])
 
 if nav == 'Home':
-    st.write('Home - but see Energy menu item')
+    st.write('Home')
+    st.write('Just A Blank Page - see Energy')
+    
+    # Header
+    st.header("Main header")
+
+    # Subheader
+    st.subheader("This is a subheader")
+
+    # Markdown
+    st.markdown("This is markdown **text**")
+    st.markdown("# Header1")
+    st.markdown("## Header 2")
+    st.markdown("### Header 3")
+
+    # Caption
+    st.caption("This is a caption")
+
+    # Code block
+    st.code("""import pandas as pd
+    pd.read_csv(my_csv_file)
+    """)
+
+    # Preformatted text
+    st.text("Some text")
+
+    # LaTeX
+    st.latex("x = 2^2")
+
+    # Divider
+    st.text('Text above divider')
+    st.divider()
+    st.text('Text below divider')
+
+    #st.write
+    st.write('Some text')
 
 if nav == 'Agriculture':
-    st.write('Agriculture - but see Energy item')
+    st.write('Agriculture')
+    st.write('Just A Blank Page - see Energy')
+    
+
 
 if nav == 'Energy':
     st.write('Energy')
